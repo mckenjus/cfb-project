6b1024959a81f3ed34249d138293a1458ec83383
Justin McKenzie <mckenjus@gmail.com>
2021-09-21 09:15:20 -0600

    second try

--- client/cfb.js
+++ client/cfb.js
@@ -11,24 +11,30 @@ let joeButton = document.getElementById('randomPlayer');
 
 
 const randomTeam = () => {
-    const results = axios.get('/teams').then((res) => {
-        console.log(res.data)
-        document.getElementById('response').innerHTML = res.data
+    const team = axios.get('/teams').then((res) => {
+        document.getElementById('team').textContent = `Team: ${res.data.school}`
+        document.getElementById('mascot').textContent = `Mascot: ${res.data.mascot}`
+        document.getElementById('location').textContent = `Stadium: ${res.data.location.name}`
+        document.getElementById('conference').textContent = `Conference: ${res.data.conference}`
     })
 }
 const randomStadium = ()=> {
-    const results = axios.get('/venues').then((res) => {
-        console.log(res.data)
-        document.getElementById('response').innerHTML = res.data
-        const myJSON = JSON.stringify(res.data);
+    const stadium = axios.get('/venues').then((res) => {
+        document.getElementById('name').textContent = `Stadium Name: ${res.data.name}`
+        document.getElementById('city').textContent = `City: ${res.data.city}`
+        document.getElementById('state').textContent = `State: ${res.data.state}`
+        document.getElementById('capacity').textContent = `Capacity: ${res.data.capacity}`
+        document.getElementById('constructed').textContent = `Year Built: ${res.data.year_constructed}`
     })
 
 }
 const randomPlayer = ()=> {
     const results = axios.get('/players').then((res) => {
-        console.log(res.data)
-        document.getElementById('response').innerHTML = res.data
-        const myJSON = JSON.stringify(res.data);
+        document.getElementById('joename').textContent = `Your Joe: ${res.data.name}`
+        document.getElementById('joeteam').textContent = `Team: ${res.data.team}`
+        document.getElementById('position').textContent = `Position: ${res.data.position}`
+        document.getElementById('jersey').textContent = `Jersey Number: ${res.data.jersey}`
+        document.getElementById('hometown').textContent = `Hometown: ${res.data.hometown}`
     })
 
 }
@@ -36,3 +42,23 @@ const randomPlayer = ()=> {
 teamButton.addEventListener('click', randomTeam)
 staduimButton.addEventListener('click', randomStadium)
 joeButton.addEventListener('click', randomPlayer)
+
+
+function createTeamCard(team) {
+    const teamCard = document.createElement('div')
+    teamCard.classList.add('team-card')
+
+    teamCard.innerHTML = `<img alt='team logo' src=${team.logos[1]} class="team-cover-image"/>
+    <p class="name">${team.school}</p>`
+    
+    
+
+
+    teamContainer.appendChild(teamCard)
+}
+
+function displayTeams(arr) {
+    teamContainer.innerHTML = ``
+    for (let i = 0; i < arr.length; i++) {
+        createTeamCard(arr[i])}
+    }
